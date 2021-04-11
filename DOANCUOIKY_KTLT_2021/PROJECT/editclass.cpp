8917d67391bc1518c23bb3ce5603b22e6de1b95f
minhphu20127593 <81699204+minhphu20127593@users.noreply.github.com>
2021-04-11 13:36:38 +0700

    ham chinh sua lop chunhiem (editclass)

---
+++ DOANCUOIKY_KTLT_2021/PROJECT/editclass.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include<string.h>
+using namespace std;
+struct Class {
+	string class_name;
+	string class_teacher; //gvcn
+	int num_of_student;
+	Class* pNext;
+};
+struct SchoolYear {
+	int begin_year;
+	int end_year;
+	Semester* pSemester;
+	SchoolYear* pNext;
+};
+
+struct Semester {
+	int no;
+	int begin_date, begin_month, begin_year;
+	int end_date, end_month, end_year;
+	Semester* pNext;
+};
+void editClass(Class*& pClass);
+
+//ham sua
+void editClass(Class*& pClass)
+{
+	string newclass_name;
+	string newclass_teacher;
+	
+	if (newclass_name != pClass->class_name)
+	{
+		cout << " Nhap lai ten lop: "; cin >> newclass_name;
+	}
+	if(newclass_teacher != pClass->class_teacher)
+	{
+		cout << " Nhap lai ten GVCN: "; cin >> newclass_teacher;
+	}
+	
+	cout << "====================================\n\n";
+	cout << "Lop: " << pClass->class_name << endl;
+	cout << "Tong hoc sinh: " << pClass->num_of_student << endl;
+	cout << "GVCN: " << pClass->class_teacher << endl;
+	cout << "Nhap 1 de chinh sua ten lop\n"
+		<< "Nhap 2 de chinh sua ten GVCN\n"
+		<< "Nhap 0 de quay lai" << endl;
+}
